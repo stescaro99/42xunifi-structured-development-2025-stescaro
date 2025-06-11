d80b064cc100dc09a1a25695592f53df335d715f
stescaro99 <stescaro@student.42firenze.it>
2025-06-11 17:03:16 +0200

    diocan

--- ex08/password_validator_bonus.c
+++ ex08/password_validator_bonus.c
@@ -41,6 +41,18 @@ static bool check_similar(const char *s, const char *t)
     return false;
 }
 
+static void safe_strcpy(char *dest, const char *src, size_t dest_size)
+{
+    if (dest_size == 0)
+        return;
+    size_t i = 0;
+    while (i < dest_size - 1 && src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
 PwStatus validate_password(const char *new_pw, PasswordHistory *history)
 {
     if (validate_password_weak(new_pw, (*history)[0]) == 1)
@@ -50,11 +62,8 @@ PwStatus validate_password(const char *new_pw, PasswordHistory *history)
         if (check_similar(new_pw, (*history)[i]))
             return 2;
     }
-    strncpy((*history)[2], (*history)[1], 1023);
-    (*history)[2][1023] = '\0';
-    strncpy((*history)[1], (*history)[0], 1023);
-    (*history)[1][1023] = '\0';
-    strncpy((*history)[0], new_pw, 1023);
-    (*history)[0][1023] = '\0';
+    safe_strcpy((*history)[2], (*history)[1], 1024);
+    safe_strcpy((*history)[1], (*history)[0], 1024);
+    safe_strcpy((*history)[0], new_pw, 1024);
     return 0;
 }
