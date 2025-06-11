9ab350013aa2c91e139d9fcda6373defaa5c0e60
stescaro99 <stescaro@student.42firenze.it>
2025-06-11 16:54:32 +0200

    mah

--- ex09/grade_map.h
+++ ex09/grade_map.h
@@ -1,20 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-typedef const char* (*GradeMapper)(int score);
+#include "../ex06/grade_map.h"
 
 typedef struct GradeNode {
     const char *grade;
     int count;
     struct GradeNode *next;
 } GradeNode;
-
-void map_scores(const int *scores, int size, GradeMapper mapper, const char **mapped_grades);
-
-const char* standard_mapper(int score);
-const char* passfail_mapper(int score);
-const char* plusminus_mapper(int score);
 
 GradeNode *compute_distribution(const char **mapped_grades, int size);
 void print_distribution(GradeNode *head);
