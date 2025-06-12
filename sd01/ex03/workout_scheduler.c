214b0bec1165b37a8be316fa7785536102f170bb
stescaro99 <stescaro@student.42firenze.it>
2025-06-12 09:20:58 +0200

    res

--- sd01/ex03/workout_scheduler.c
+++ sd01/ex03/workout_scheduler.c
@@ -5,7 +5,6 @@ struct WorkoutPlan *create_workout_schedule(char *username)
     struct UserData *user = NULL;
     struct WorkoutPlan *plan = NULL;
     int duration = 0;
-    int day;
     user = get_user_data(username);
     if (!user)
         return NULL;
@@ -18,7 +17,7 @@ struct WorkoutPlan *create_workout_schedule(char *username)
     duration = determine_duration(plan);
     if (duration <= 0)
         return (free_user_data(user), free_workout_plan(plan), NULL);
-    for (day = 1; day <= duration; ++day)
+    for (int day = 1; day <= duration; ++day)
     {
         assign_daily_exercises(plan, day);
         assign_daily_tips(plan, day);
