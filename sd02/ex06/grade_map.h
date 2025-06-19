1113dadee775384e150336ce9eb696687dd76489
stescaro99 <stescaro@student.42firenze.it>
2025-06-19 09:45:03 +0200

    duplicati rimossi

--- sd02/ex06/grade_map.h
+++ sd02/ex06/grade_map.h
@@ -1,12 +1,10 @@
 #ifndef GRADE_MAP_H
 #define GRADE_MAP_H
 
+#include "../../utils/utils.h"
+
 typedef const char* (*GradeMapper)(int score);
 
 void map_scores(const int *scores, int size, GradeMapper mapper, const char **mapped_grades);
-
-const char* standard_mapper(int score);
-const char* passfail_mapper(int score);
-const char* plusminus_mapper(int score);
 
 #endif
--- utils/utils.h
+++ utils/utils.h
@@ -7,5 +7,8 @@ int ft_strlen(const char *s);
 int strcasestr_custom(const char *haystack, const char *needle);
 bool diff_pass(const char *s, const char *t);
 bool check_cases(const char *s);
+const char* plusminus_mapper(int score);
+const char* passfail_mapper(int score);
+const char* standard_mapper(int score);
 
 #endif
